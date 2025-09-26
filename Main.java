f1c8933e138ed7a474d9de17119973f40d98c6fa
akshayaraja2007 <akshayarajae@gmail.com>
2025-09-26 11:09:24 +0530

    first commit

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-class Main{
-    public static void main(String[] args){
-        System.out.println("hello world");
+public class Main {
+    public static void main(String[] args) {
+        int rows = 5; // You can change this value to print more or fewer rows
+
+        for (int i = 1; i <= rows; i++) {
+            // Print stars for each row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
     }
 }
