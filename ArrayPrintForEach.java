9a82256b9151a23402dd8bc1317366829ee88720
AKSHAYARAJA <akshayarajae@gmail.com>
2025-09-27 11:59:57 +0530

    array

---
+++ ArrayPrintForEach.java
@@ -0,0 +1,10 @@
+public class ArrayPrintForEach {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Mango"};
+
+        System.out.println("Fruits in the array:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
